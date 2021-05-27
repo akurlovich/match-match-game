d8167884f2e7daec67a37614ed1221e50cfe7688
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-05-27 14:43:15 +0300

    feat: save settiongs

--- src/ts/settings_page/select_block.ts
+++ src/ts/settings_page/select_block.ts
@@ -3,14 +3,14 @@ import { OptionSelect } from "./options";
 
 export class SelectBlockCategory extends Control {
   firstOption = new OptionSelect(this.element, '', 'select game cards type', true, true);
-  firstOption1 = new OptionSelect(this.element, 'pets', 'pets', false, false);
-  firstOption2 = new OptionSelect(this.element, 'country', 'country', false, false);
+  firstOption1 = new OptionSelect(this.element, '1', 'pets', false, false);
+  firstOption2 = new OptionSelect(this.element, '2', 'country', false, false);
 }
 
 export class SelectBlockDifficulty extends Control {
   firstOption = new OptionSelect(this.element, '', 'select game cards', true, true);
-  firstOption1 = new OptionSelect(this.element, '4', '4x4', false, false);
-  firstOption2 = new OptionSelect(this.element, '6', '6x6', false, false);
-  firstOption3 = new OptionSelect(this.element, '8', '8x8', false, false);
+  firstOption1 = new OptionSelect(this.element, '2', '4x2', false, false);
+  firstOption2 = new OptionSelect(this.element, '3', '4x3', false, false);
+  firstOption3 = new OptionSelect(this.element, '4', '4x4', false, false);
 
 }
--- src/ts/settings_page/settings_wrapper.ts
+++ src/ts/settings_page/settings_wrapper.ts
@@ -22,12 +22,21 @@ export class SettingsWrapper extends Control {
     this.selectedContainerGameType = new Control(this.element, 'div', 'setting__game-type');
     this.h3ElementGameType = new Control(this.selectedContainerGameType.element, 'h3', 'settings__title', 'Game cards');
     this.selectedDivGameType = new SelectBlockCategory(this.selectedContainerGameType.element, 'select', 'settings__options');
-    console.log((this.selectedDivGameType.element as HTMLInputElement).value)
+    this.selectedDivGameType.element.onchange = () => {
+      let category = (this.selectedDivGameType.element as HTMLInputElement).value;
+      localStorage.setItem('category', category)
+      console.log((this.selectedDivGameType.element as HTMLInputElement).value)
+    }
   
     //  -----второй селект-----
     this.selectedContainerDificulty = new Control(this.element, 'div', 'settings__difficulty');
     this.h3ElementDificulty = new Control(this.selectedContainerDificulty.element, 'h3', 'settings__title', 'Difficulty');
     this.selectedDivDificulty = new SelectBlockDifficulty(this.selectedContainerDificulty.element, 'select', 'settings__options');
+    this.selectedDivDificulty.element.onchange = () => {
+      let difficulty = (this.selectedDivDificulty.element as HTMLInputElement).value;
+      localStorage.setItem('difficulty', difficulty)
+      console.log((this.selectedDivDificulty.element as HTMLInputElement).value)
+    }
   
   }
 }
